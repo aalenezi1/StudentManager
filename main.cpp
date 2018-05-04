b864064b14b75f7dce4a9e846c9e8999ca4198c5
aalenezi1 <aalenezi1@mail.csuchico.edu>
2018-05-03 21:12:45 -0700

    final work

--- main.cpp
+++ main.cpp
@@ -6,16 +6,30 @@ using namespace std;
 
 int main()
 {
-    string firstName;
-    string lastName;
-    int number;
-    student person;
-    
-    cout<<"What is the student's name? ";
-    cin>>firstName>>lastName;
-    
-    person.setName(firstName,lastName);
-    cout<<"Saved "<<person.fullName()<<endl;
-    
-    return 0;
+        string firstName;
+        string lastName;
+        student person;
+
+        cout<<"What is the student's name? ";
+        cin>>firstName>>lastName;
+
+        person.setName(firstName,lastName);
+
+        double grade, score;
+        do
+        {
+                cout<<"Add grade (or -1 to finish): ";
+                cin>>grade;
+                if( grade != -1 )
+                {
+                        person.addGrade(grade);
+                }
+        }while( grade != -1 );
+
+        score = person.getScore();
+
+        cout<<person.fullName()<<"'s Course grade is: "<<score<<endl;
+
+        return 0;
 }
+
